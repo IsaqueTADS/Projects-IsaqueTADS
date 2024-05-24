860cfa4de8d5c5e585dfd513d6b2cd1c26a230e9
IsaqueTADS <isaqueofc18@gmail.com>
2024-05-24 02:22:41 -0300

    mod1

--- C++/Calculdaora_de_IMC_2.0.cpp
+++ C++/Calculdaora_de_IMC_2.0.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<locale.h>
+#include<stdbool.h>
 using namespace std;
 int main (){
     setlocale(LC_ALL,"portuguese");
-    float altura, peso,imc,opc;
+    float altura, peso,imc;
+    int opc;
 
 
     cout<<"CALCULADORA DE IMC 2.0\n"<<endl;
@@ -14,12 +16,28 @@ int main (){
     cout<<" [1] Calcular IMC\n [2] Sair\n Escolha uma das opções! "<<endl;
     cin>>opc;
 
+    switch(opc){
+
+        case 1:
+              system("cls");
+              cout<<"Qual a sua altura? "<<endl;
+              cin>>altura;
+              cout<<"Qual o seu peso? "<<endl;
+              cin>>peso;
+              imc=peso/(altura*altura);
+              
+              break;
+    }
+
+    while(opc != 2 )
+    {
+
+        break;
+
+    }
 
 
-    cout<<"Qual a sua altura? "<<endl;
-    cin>>altura;
-    cout<<"Qual o seu peso? "<<endl;
-    cin>>peso;
+
 
 
 
