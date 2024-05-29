aca77b8d065b465fae09882aa1964f7f0cfa0355
IsaqueTADS <isaqueofc18@gmail.com>
2024-05-29 18:40:54 -0300

    Versão 1.0

---
+++ C++/Jogo_da_forca_basico.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <locale.h>
+#include <stdbool.h>
+
+using namespace std;
+
+int main(){
+
+    setlocale(LC_ALL,"portuguese_Brazil");
+
+    char palavra[60], chute[1], forca[60];
+    int opc, indice, tamanho, vidas, acertos;
+    bool acerto;
+
+
+
+    cout<<"JOGO DA FOCAR V1.0 "<<endl;
+    cout<<"project by Isaque Rodrigues Alves"<<endl;
+    system("pause");
+
+    cout<<"[1] Jogar";
+    cout<<"[2] Sair";
+    cout<<"Escolha uma das opções acima! "<<endl;
+    cin>>opc;
+
+    switch(opc){
+        
+        case 1:
+
+         indice = 0;
+         tamanho = 0;
+    
+    }
+
+    while(opc == 2){
+        break;
+    }
+
+
+    
+
+   
+
+ 
+
+
+
+
+
+
+    return 0;
+}
